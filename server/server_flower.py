9ce241c347e64eab3c57a3fed69a13bf3e898541
afanatif <121168351+afanatif@users.noreply.github.com>
2025-11-21 00:07:41 +0500

    Update server_flower.py

--- server/server_flower.py
+++ server/server_flower.py
@@ -1,8 +1,7 @@
-# server/server_flower.py
 import flwr as fl
 
 if __name__ == "__main__":
     fl.server.start_server(
         server_address="localhost:8080",
-        config={"num_rounds": 3}  # just pass as a dict
+        num_rounds=3  # pass directly, NOT inside a dict
     )
