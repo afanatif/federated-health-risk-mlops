7b8a15b463c7666287cd77060709199c0c23b401
afanatif <121168351+afanatif@users.noreply.github.com>
2025-11-20 23:17:40 +0500

    Update server_flower.py

--- server/server_flower.py
+++ server/server_flower.py
@@ -2,5 +2,5 @@
 import flwr as fl
 
 if __name__ == "__main__":
-    # Simple Flower server with 3 rounds for demo
+    # Start Flower server with 3 rounds (adjust as needed)
     fl.server.start_server(config={"num_rounds": 3})
