1c17ffa1a1eb97d4d3a4e78cb551ab4de4735ac0
afanatif <121168351+afanatif@users.noreply.github.com>
2025-11-20 23:59:16 +0500

    Update server_flower.py

--- server/server_flower.py
+++ server/server_flower.py
@@ -1,12 +1,9 @@
 # server/server_flower.py
 import flwr as fl
-from flwr.server.server import ServerConfig  # correct import
+from flwr.server.server_config import ServerConfig  # correct import
 
 if __name__ == "__main__":
-    # Create a server config
     config = ServerConfig(num_rounds=3)
-
-    # Start Flower server
     fl.server.start_server(
         server_address="localhost:8080",
         config=config
