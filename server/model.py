7e2e1ee781d9365549b76309455735b684fe1f7e
afanatif <121168351+afanatif@users.noreply.github.com>
2025-11-20 23:22:18 +0500

    Update model.py

--- server/model.py
+++ server/model.py
@@ -2,17 +2,32 @@
 import torch.nn as nn
 import torch
 
-class RiskModel(nn.Module):
-    def __init__(self, input_dim=5):
+class SimpleCNN(nn.Module):
+    def __init__(self):
         super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(input_dim, 64),
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(16),
             nn.ReLU(),
-            nn.Linear(64,32),
+            nn.MaxPool2d(2),  # 112
+            nn.Conv2d(16, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.Linear(32,1),
+            nn.MaxPool2d(2),  # 56
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool2d((1,1))
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 1),
             nn.Sigmoid()
         )
 
     def forward(self, x):
-        return self.net(x)
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
